06bd343f244d7cca5be63c6f96a063a42587cb7b
joycevnr <joycevitoria150@gmail.com>
2024-06-29 01:10:13 -0300

    add comment

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -4,7 +4,7 @@ import App from './App'; // Importando o componente principal da aplicação
 
 // Renderizando o aplicativo dentro do elemento com id 'root'
 ReactDOM.render(
-  <React.StrictMode>{/*ferramenta para chegar possíveis más práticas no código react, caso encontre potênciais problemas, emite warnings -(apenas no ambiente de desenvolvimento)*/}
+  <React.StrictMode>{/*ferramenta para chegar possíveis más práticas no código react, caso encontre potênciais problemas, emite warnings -(apenas no ambiente de desenvolvimento)- roda duas vezes oq pode até ajudar a capturar bugs*/}
     <App /> {/* Renderizando o componente principal App dentro do StrictMode */}
   </React.StrictMode>, 
   document.getElementById('root') // Selecionando o elemento HTML com id 'root'
